17752f325080bc6ec812c4bcfd0e3f2f7c3cf554
MAHANNAVIDU734 <mahan.navidu@gmail.com>
2022-09-05 07:21:59 +0530

    error status

--- api/index.js
+++ api/index.js
@@ -22,6 +22,8 @@ mongoose.connection.on("disconnected", () => {
         console.log("mongoDB Disconnected!")
     })
     //middleware
+
+
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -29,7 +31,18 @@ app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-app.listen(3000, () => {
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+app.listen(2379, () => {
     connect()
     console.log("Connected to Backend.");
 });
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,17 +5,55 @@ const router = express.Router();
 
 router.post("/", async(req, res) => {
 
-        const newHotel = new Hotel(req.body)
+    const newHotel = new Hotel(req.body)
 
-        try {
-            const savedHotel = await newHotel.save()
-            res.status(200).json(savedHotel)
-        } catch (err) {
-            res.status(500).json(err)
-        }
-    })
-    //UPDATE
-    //DELETE
-    //GET
-    //GET_ALL
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+//UPDATE
+router.put("/:id", async(req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedHotel);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+//DELETE
+router.delete("/:id", async(req, res) => {
+    try {
+        const deleteHotel = await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel Has Been Deleted");
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+//GET
+router.get("/:id", async(req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//GET_ALL
+router.get("/", async(req, res, next) => {
+    const failed = true
+    const err = new Error()
+    err.status = 404;
+    err.message = "Sorry not found!";
+    if (failed) return next(err);
+    try {
+        const hotels = await Hotel.findById("asdsdas");
+        res.status(200).json(hotels);
+    } catch (err) {
+        next(err)
+    }
+});
+
 export default router
